ffe5d80897af42101d983cd80983b0761e6b9824
Ameeramahnoor <ameeramahnoor000@gmail.com>
2025-04-21 12:38:35 +0530

    Add initial files

---
+++ routes/review.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const router=express.Router({mergeParams:true});
+const wrapAsync=require("../utils/wrapAsync.js");
+const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
+const Review=require("../Models/review.js");
+const Listing=require("../Models/listing.js");
+const reviewController=require("../controllers/reviews.js");
+
+
+//Reviews
+//post route
+router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview));
+
+//delete review route
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+module.exports=router;
